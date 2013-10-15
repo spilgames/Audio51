a1f8e415f77427ea90479b3886a4c13654c38c68
martin-spil-reurings <Martin.Reurings@spilgames.com>
2013-10-15 15:05:14 +0200

    Docs!

--- src/audio51.js
+++ src/audio51.js
@@ -1,13 +1,25 @@
 /*global define, RSVP*/
-define( ["webaudio/context","audiotag/context"], function(wac, atc) {
+define( ["webaudio/context","audiotag/context","unrestrict"], function(wac, atc, unrestrict) {
     'use strict';
 
     return {
+        /**
+         * Get an `AudioContext` audio51 style. This method will figure out which scenario fits
+         * the current environment best. In case you find a use-case where you need to overrule
+         * this automation, you can provide an override.
+         * Internal caching will ensure that you can always quickly call this method to retreive
+         * an `AudioContext`, override will always break the cache, but on subsequent calls will
+         * be cached if the override parameter is omitted.
+         * 
+         * @param {int} override Force this method to return a context of your own chosing.
+         */
         getContext: ( function() {
             var ctx = null;
-            
+
+            //Determine override type, if provided.
             return function( override ) {
                 if ( override ) {
+
                     switch (override) {
                         case 2:
                             ctx = atc
@@ -17,13 +29,26 @@ define( ["webaudio/context","audiotag/context"], function(wac, atc) {
                             ctx = wac
                             break;
                     }
+
                 } else if (ctx === null) {
+
+                    //If WebAudio API is available it should be used
                     if ( wac.canIUse() ) {
+                        //WebAudio API has internal 'arming' so mobile and desktop are the same
                         ctx = wac;
                     } else {
-                        ctx = atc;
+
+                        //Check for touch, touch probably means mobile, acceptable margin of error
+                        if ( unrestrict.isTouch() ) {
+                            ctx = atc; //TODO: Load restricted set (audiosprite)
+                        } else {
+                            ctx = atc;
+                        }
+
                     }
                 }
+
+                //Return cached context
                 return ctx;
             };
         }())
