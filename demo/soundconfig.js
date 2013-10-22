d04ac701fa6b9fe98394b1b92b8b984a726f3be3
martin-spil-reurings <Martin.Reurings@spilgames.com>
2013-10-22 15:52:26 +0200

    Quick and easy demo

---
+++ demo/soundconfig.js
@@ -0,0 +1,47 @@
+/*global define, RSVP*/
+/**
+ * Soundset generated using `audiosprite -p mp3,ogg,ac3 -o sounds -e mp3,ogg,ac3 -c 2 *.ogg`
+ */
+require(["audio51"], function(audio51) {
+    
+    var boom = null,
+        l1 = document.getElementById("l1"),
+        l2 = document.getElementById("l2");
+
+    audio51.loadSoundSet('sounds.json');
+    
+    function stop() {
+        if (boom) {
+            boom.stop();
+        }
+    }
+    
+    function start() {
+        boom = audio51.play("thunder");
+    }
+    
+    function addListeners(type) {
+        l1.addEventListener(type, start);
+        l2.addEventListener(type, stop);
+    }
+    
+    function addTouch() {
+        addListeners("touchstart");
+        document.documentElement.removeEventListener("touchstart", addTouch);
+    }
+    
+    function addClick() {
+        addListeners("click");
+        document.documentElement.removeEventListener("click", addClick);
+    }
+
+    document.getElementById("a1").addEventListener("click", function(){
+        document.getElementById("playnow").className += " hide";
+        return false;
+    });
+
+
+    document.documentElement.addEventListener("click", addClick);
+    document.documentElement.addEventListener("touchstart", addTouch);
+
+});
